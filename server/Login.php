30a5cb0387c64fba8ebf319d525416ddfe766ce3
laila-elhattab <liko.hani@gmail.com>
2021-12-24 15:19:04 +0200

    initial commit

---
+++ server/Login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+?>
+<html>
+<script src="validations.js"></script>
+<?php
+include 'database.php';
+
+if (isset($_POST['login'])) {
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+        for ($i = 0; $i < 4; $i++) {
+            $sql = "Select * from " . $user[$i] . " where email ='" . $_POST["email"] . "' and password='" . $_POST["password"] . "'";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                $_SESSION["id"] = $row["id"];
+                $_SESSION["name"] = $row["name"];
+                $_SESSION["email"] = $row["email"];
+                $_SESSION["password"] = $row["password"];
+                if (isset($_POST['remember'])) {
+                    $_SESSION['start'] = time();
+                    $_SESSION['expire'] = $_SESSION['start'] + (3 * 24 * 60 * 60);
+                }
+                header("Location:home.php");
+                break;
+            }
+            if ($i == 3) {
+?>
+                <label>Invalid Login, please try again</label><br><br>
+        <?php
+            }
+        }
+    } else {
+        ?>
+        <label>Please enter a valid email</label><br><br>
+<?php
+    }
+}
+include "server/login.html";
+?>
+
+
+</html>
---
+++ server/validations.js
@@ -0,0 +1,59 @@
+function validateLog(form) {
+    var fail = "";
+    if (form.email.value == "") {
+        fail += "Please Enter your email";
+    }
+    if (form.password.value == "") {
+        fail += "Please Enter your password";
+    }
+    if (fail == "") {
+        return true;
+    }
+    else {
+        alert(fail);
+        return false;
+    }
+}
+
+function validateEdit(form) {
+    var fail = "";
+    if (form.email.value == "") {
+        fail += "Please Enter your email";
+    }
+    if (fail == "") {
+        return true;
+    }
+    else {
+        alert(fail);
+        return false;
+    }
+}
+function validateCourse(form) {
+    var fail = "";
+    if (form.name.value == "") {
+        fail += 1;
+    }
+    if (form.price.value == "") {
+        fail += 2;
+    }
+    if (form.des.value == "") {
+
+    }
+    if (form.imageToUpload.value == "") {
+        fail += 3;
+    }
+    if (form.contentToUpload.value == "") {
+        fail += 4;
+    }
+    if (fail == "") {
+        return true;
+    }
+    else {
+        alert(fail);
+        return false;
+    }
+
+}
+
+
+
