350dbf35a4307e007f809f11b307340d0f687127
laila-elhattab <liko.hani@gmail.com>
2021-12-26 23:20:44 +0200

    initial commit

--- server/editpwd.php
+++ server/editpwd.php
@@ -1,43 +1,44 @@
 <?php
 session_start();
 ?>
-
+<html>
+<h3>Forgot Password</h3>
 <?php
-include 'database.php';
-include 'mail.php';
-$user = array("learner", "administrator", "auditor", "tutor");
+include_once 'database.php';
+include_once 'mail.php';
+
 
 if (isset($_POST['reset'])) {
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
         for ($i = 0; $i < 4; $i++) {
-            $sql = "Select email from " . $user[$i] . " where email ='" . $_POST["email"] . "'";
+            $sql = "Select email,security_ans from " . $user[$i] . " where email ='" . $_POST["email"] . "'";
             $result = $conn->query($sql);
+            $row = $result->fetch_assoc();
             if ($result->num_rows > 0) {
-                emailPwd($_POST["email"], $user[$i]);
-                header("Location:login.php");
-                break;
-            }
-            if ($i == 3) {
+                if ($_POST['security'] == $row['security_ans']) {
+                    emailPwd($_POST["email"], $user[$i]);
+                    header("Location:login.php");
+                    break;
+                } else {
 ?>
-                <label>Invalid Email, please try again</label><br><br>
-
+                    <label>Your security answer is incorrect</label><br>
+                <?php
+                    break;
+                }
+            } else {
+                ?>
+                <label>Invalid Email, please try again</label><br>
         <?php
+                break;
             }
         }
     } else {
         ?>
-        <label>Invalid Email, please try again</label><br><br>
+        <label>Invalid Email, please try again</label><br>
 <?php
     }
 }
+include_once $_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/editpwd.html";
 ?>
-<html>
-<script src="validations.js"></script>
-<h3>Forgot Password</h3>
-<form method="POST" action="editpwd.php" onsubmit="return validateEdit(this)">
-    <p>Please enter your email address</p>
-    <input type="text" name="email" placeholder="Email address"></input><br><br>
-    <input type="submit" name="reset" value="Reset Password">
-</form>
 
 </html>
