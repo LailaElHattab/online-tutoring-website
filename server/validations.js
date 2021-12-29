f3df1fecb0ddd94e08cb7253c866fb696f077120
laila-elhattab <liko.hani@gmail.com>
2021-12-30 00:25:57 +0200

    course validation

--- server/validations.js
+++ server/validations.js
@@ -34,19 +34,26 @@ function validateEdit(form) {
 function validateCourse(form) {
     var fail = "";
     if (form.name.value == "") {
-        fail += 1;
+        fail += "Please Enter the course name ";
     }
     if (form.price.value == "") {
-        fail += 2;
-    }
-    if (form.des.value == "") {
 
+        fail += "Please Enter a valid course price ";
+    }
+    if (form.category.value == "") {
+        fail += "Please specify the course category ";
+    }
+    if (form.tutor.value == "") {
+        fail += "Please Enter the tutor email ";
     }
     if (form.imageToUpload.value == "") {
-        fail += 3;
+        fail += "Please Upload the course image ";
+    }
+    if (form.desToUpload.value == "") {
+        fail += "Please Enter the course description ";
     }
     if (form.contentToUpload.value == "") {
-        fail += 4;
+        fail += "Please Upload the course content ";
     }
     if (fail == "") {
         return true;
