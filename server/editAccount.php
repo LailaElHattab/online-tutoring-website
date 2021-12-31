70beb93ab597aca58ee2ae229e22a5bb555b1881
laila-elhattab <liko.hani@gmail.com>
2021-12-31 16:04:22 +0200

    initial commit

--- server/editAccount.php
+++ server/editAccount.php
@@ -4,6 +4,13 @@ session_start();
 <html>
 
 <?php
+// if (isset($_POST['email'])) {
+//     $sql = "UPDATE " . $_SESSION['user'] . " SET email='" . $_POST['email'] . "' WHERE id='" . $_SESSION['id'] . "'";
+//     $conn->query($sql);
+// } else if (isset($_POST['pwd'])) {
+//     $sql = "UPDATE " . $_SESSION['user'] . " SET password='" . $_POST['pass'] . "' WHERE id='" . $_SESSION['id'] . "'";
+//     $conn->query($sql);
+// }
 include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/editAccount.html");
 ?>
 
