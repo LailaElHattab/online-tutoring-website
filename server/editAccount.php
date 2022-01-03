13cef1a7c7f71208a425b4aea25a9caaad11bde2
laila-elhattab <liko.hani@gmail.com>
2022-01-03 19:46:17 +0200

    adjusted the alerts

--- server/editAccount.php
+++ server/editAccount.php
@@ -4,15 +4,57 @@ session_start();
 <html>
 
 <?php
-// if (isset($_POST['email'])) {
-//     $sql = "UPDATE " . $_SESSION['user'] . " SET email='" . $_POST['email'] . "' WHERE id='" . $_SESSION['id'] . "'";
-//     $conn->query($sql);
-// } else if (isset($_POST['pwd'])) {
-//     $sql = "UPDATE " . $_SESSION['user'] . " SET password='" . $_POST['pass'] . "' WHERE id='" . $_SESSION['id'] . "'";
-//     $conn->query($sql);
-// }
+include_once 'database.php';
 include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/editAccount.html");
-?>
 
+if (isset($_POST['emailbtn'])) {
+    $sql1 = "SELECT * FROM user where email='" . $_POST['email'] . "'";
+    $result = $conn->query($sql1);
+    if ($result->num_rows > 0) {
+?>
+        <script>
+            el1 = document.createElement("div");
+            el1.setAttribute("class", "alert alert-danger col-md-4 ");
+            el1.setAttribute("style", "width:400px;");
+            el1.innerHTML = "This Email has already been taken!";
+            document.getElementById("emailPart").prepend(el1);
+        </script>
+    <?php
+    } else {
+        $sql2 = "UPDATE user SET email='" . $_POST['email'] . "' WHERE id='" . $_SESSION['id'] . "'";
+        $conn->query($sql2);
+        $_SESSION['email'] = $_POST['email'];
+    ?>
+        <script>
+            el1 = document.createElement("div");
+            el1.setAttribute("class", "alert alert-success col-md-4 ");
+            el1.setAttribute("style", "width:400px;");
+            el1.innerHTML = "Your email has been changed successfully!";
+            document.getElementById("emailPart").prepend(el1);
+        </script>
+    <?php
+    }
+}
+if (isset($_POST['pwdbtn'])) {
+    $sql = "UPDATE user SET password='" . $_POST['password'] . "' WHERE id='" . $_SESSION['id'] . "'";
+    $conn->query($sql);
+    $_SESSION['password'] = $_POST['password'];
+    ?>
+    <script>
+        el1 = document.createElement("div");
+        el1.setAttribute("class", "alert alert-success col-md-4 ");
+        el1.setAttribute("style", "width:400px;");
+        el1.innerHTML = "Your Password has been changed successfully!";
+        document.getElementById("emailPart").prepend(el1);
+    </script>
+<?php
+}
+
+
+?>
+<script type="text/javascript">
+    document.getElementById('email').value = '<?php echo $_SESSION['email']; ?>';
+    document.getElementById('pw').value = '<?php echo $_SESSION['password']; ?>';
+</script>
 
 </html>
