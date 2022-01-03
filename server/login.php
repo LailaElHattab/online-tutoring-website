594bda689496ddf4c2c23837c80bb93e6a359e2c
laila-elhattab <liko.hani@gmail.com>
2022-01-03 20:01:01 +0200

    added alerts

--- server/login.php
+++ server/login.php
@@ -5,6 +5,7 @@ session_start();
 <script src="validations.js"></script>
 <?php
 include_once 'database.php';
+include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/login.html");
 if (isset($_POST['login'])) {
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
         $sql = "Select * from user where email ='" . $_POST["email"] . "' and password='" . $_POST["password"] . "'";
@@ -14,26 +15,38 @@ if (isset($_POST['login'])) {
             $_SESSION["id"] = $row["id"];
             $_SESSION["name"] = $row["fname"];
             $_SESSION["email"] = $row["email"];
+            $_SESSION["password"] = $row["password"];
             $_SESSION["user"] = $row["type"];
             if (isset($_POST['remember'])) {
                 $_SESSION['start'] = time();
                 $_SESSION['expire'] = $_SESSION['start'] + (3 * 24 * 60 * 60);
             }
             header("Location:home.php");
-        }
-        if ($i == 3) {
+        } else {
 ?>
-            <label>Invalid Login, please try again</label><br><br>
+            <script>
+                el1 = document.createElement("div");
+                el1.setAttribute("class", "alert alert-danger col-md-4 ");
+                el1.setAttribute("style", "width:400px;");
+                el1.innerHTML = "Invalid Login, please try again!";
+                document.getElementById("emailInput").prepend(el1);
+            </script>
         <?php
         }
     } else {
         ?>
-        <label>Please enter a valid email</label><br><br>
+        <script>
+            el1 = document.createElement("div");
+            el1.setAttribute("class", "alert alert-danger col-md-4 ");
+            el1.setAttribute("style", "width:400px;");
+            el1.innerHTML = "Please enter a valid email";
+            document.getElementById("emailInput").prepend(el1);
+        </script>
 <?php
     }
 }
 
-include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/login.html");
+
 
 
 
