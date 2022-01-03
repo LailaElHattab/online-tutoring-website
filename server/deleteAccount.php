e986beeef7803d3c899b451e0ca7ecdca45a0338
laila-elhattab <liko.hani@gmail.com>
2022-01-03 03:28:12 +0200

    updated with new database

--- server/deleteAccount.php
+++ server/deleteAccount.php
@@ -5,7 +5,7 @@ session_start();
 <?php
 include_once 'database.php';
 
-$sql = "DELETE FROM " . $_SESSION["user"] . " WHERE id='" . $_SESSION['id'] . "'";
+$sql = "DELETE FROM user WHERE id='" . $_SESSION['id'] . "'";
 $conn->query($sql);
 include_once 'signOut.php';
 
