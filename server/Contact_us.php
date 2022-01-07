aa9d248d6276328ec9e208c2ab1caa8d784f32c3
Hagar11783 <86372599+Hagar11783@users.noreply.github.com>
2022-01-07 13:51:18 +0200

    Update Contact_us.php

--- server/Contact_us.php
+++ server/Contact_us.php
@@ -6,7 +6,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
-        <link href='custom.css' rel='stylesheet' type='text/css'>
+        <link href='style.css' rel='stylesheet' type='text/css'>
     </head>
     <body>
         <div class="container">
