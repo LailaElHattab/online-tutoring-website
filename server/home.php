4e30002ae7174e37da2f37f82d66beb2afedbe1f
laila-elhattab <liko.hani@gmail.com>
2022-01-13 22:24:18 +0200

    initial commit

--- server/home.php
+++ server/home.php
@@ -1,8 +1,29 @@
+<?php
+session_start();
+?>
 <html>
 
 <?php
-include_once 'navigationBar.php';
-include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/home.html");
+
+if (!empty($_SESSION['id']) && $_SESSION["user"] == 1) {
+    //if logged in
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/navbarAdmin.html");
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/server/homeAdmin.php");
+} else if (!empty($_SESSION['id']) && $_SESSION["user"] == 2) {
+    //include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/.html");
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/navbarLearner.html");
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/home.html");
+} else if (!empty($_SESSION['id']) && $_SESSION["user"] == 3) {
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/navbarLearner.html");
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/home.html");
+} else if (!empty($_SESSION['id']) && $_SESSION["user"] == 4) {
+
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/hometutor.html");
+} else {
+    //if not logged in
+    include_once 'navigationBar.php';
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/home.html");
+}
 ?>
 
 </html>
