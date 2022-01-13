f40cb4e834b89bcdb14eca992b7fb52d97f31b95
Yosr Gamal <yosr.elqott@gmail.com>
2022-01-13 22:30:08 +0200

    included navbar for tutor

--- server/home.php
+++ server/home.php
@@ -5,19 +5,23 @@ session_start();
 
 <?php
 
+//admin
 if (!empty($_SESSION['id']) && $_SESSION["user"] == 1) {
     //if logged in
     include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/navbarAdmin.html");
     include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/server/homeAdmin.php");
+    //learner
 } else if (!empty($_SESSION['id']) && $_SESSION["user"] == 2) {
     //include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/.html");
     include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/navbarLearner.html");
     include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/home.html");
+    //auditor
 } else if (!empty($_SESSION['id']) && $_SESSION["user"] == 3) {
     include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/navbarLearner.html");
     include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/home.html");
+    //tutor
 } else if (!empty($_SESSION['id']) && $_SESSION["user"] == 4) {
-
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/navbarLearner.html");
     include_once($_SERVER['DOCUMENT_ROOT'] . "/online-tutoring-website/client/hometutor.html");
 } else {
     //if not logged in
