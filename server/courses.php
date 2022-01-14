c83ea3a55c51f54e87286d3f78a9a8a715a940f5
salma ahmed <salma1910225@miuegypt.edu.eg>
2022-01-14 20:16:25 +0200

    courses bootstrap and css

--- server/courses.php
+++ server/courses.php
@@ -2,11 +2,29 @@
 session_start();
 ?>
 <html>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<head>
+    <meta name="viewport" content="width=device-width , initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
 <style>
     .checked {
         color: orange;
     }
+    h3   {color: rgb(58, 99, 151);
+        margin-left: 20px;}
+        h1   {color: #363EE0;
+        font-style: italic;
+        margin-left: 20px;}
+        b{margin-left: 20px;}
 </style>
 <?php
 //if no content -> coming soon
@@ -29,13 +47,13 @@ echo "<img src='" . $row['image'] . "'>";
 $sql0 = "SELECT fname FROM user where id='" . $row['tutor_id'] . "'";
 $result0 = $conn->query($sql0);
 $row0 = $result0->fetch_assoc();
-echo "<p>Created by " . $row0['fname'] . "</p>";
+echo "<p><b><i>Created by " . $row0['fname'] . "</i></b></p>";
 $path = $row['description'];
 echo "<h3>Course Description</h3>";
 $file = fopen($path, "r");
 while (!feof($file)) {
     $line = fgets($file);
-    echo $line . "<br>";
+    echo "<b>$line</b>" . "<br>";
 }
 fclose($file);
 if ($_SESSION['user'] == '2' || $_SESSION['user'] = "") {
@@ -48,25 +66,29 @@ if ($_SESSION['user'] == '2' || $_SESSION['user'] = "") {
         $file1 = fopen($content, "r");
         while (!feof($file1)) {
             $line1 = fgets($file1);
-            echo $line1 . "<br>";
+            echo "<b>$line1</b>" . "<br>";
         }
     } else {
 ?>
-        <button onclick="location.href='cart.php?id=<?php echo $row['id'] ?>'">Add to cart</button>
-    <?php
+        <button class="btn btn-sm" id="editcbtn" onclick="location.href='cart.php?id=<?php echo $row['id'] ?>'">Add to cart</button>
+    
+    
+        <?php
     }
 } else {
 
     $content = $row['content'];
-    echo "<h3>Course content</h3>";
+     echo "<h3>Course content</h3>";
     $file1 = fopen($content, "r");
     while (!feof($file1)) {
         $line1 = fgets($file1);
-        echo $line1 . "<br>";
+        echo "<b>$line1</b>" . "<br>";
     }
     ?>
-    <button onclick="location.href='editCourse.php?id=<?php echo $row['id'] ?>'">edit course</button>
-    <button onclick="location.href='deleteCourse.php?id=<?php echo $row['id'] ?>'">delete course</button>
+   
+    
+    <button class="btn btn-sm" id="editcbtn" onclick="location.href='editCourse.php?id=<?php echo $row['id'] ?>'">edit course</button>
+    <button class="btn btn-sm" id="deletecbtn" onclick="location.href='deleteCourse.php?id=<?php echo $row['id'] ?>'">delete course</button>
    
 
 <?php
