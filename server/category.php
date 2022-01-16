3b38c0ec9576e06b3dcddb187591030bfdf2eee8
Yosr Gamal <yosr.elqott@gmail.com>
2022-01-16 19:59:48 +0200

    added bootstrap with css

--- server/category.php
+++ server/category.php
@@ -2,41 +2,46 @@
 session_start();
 ?>
 <html>
-
+<head>
+  <meta name="viewport" content="width=device-width , initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="styles/styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 <?php
 include_once 'database.php';
+include_once 'navbar.php';
+?>
+<body >
+
+<?php
 
 $sql1 = "SELECT * FROM course WHERE category=". $_GET['id'];
 $query = $conn->query($sql1);
 
 while($row = $query->fetch_assoc()){
 ?>
-        <div class="card mb-4" id="card">
-           
-            <div class="card-body">
-                <!-- Single item -->
-                <div class="row">
-                    <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
-                        <!-- Image -->
-                            <div class="bg-image hover-overlay hover-zoom ripple rounded" data-mdb-ripple-color="light">
-                             <img src=<?php echo $row['image']?> class="w-100"/>
-                            </div>
-                        <!-- Image -->
-                    </div>
 
-                    <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
-                        <!-- Data -->
-                        <p><strong><?php echo $row['name']?></strong></p>
-                        <!-- Data -->
-                    </div>
+<div class="card my-3" style="width: 1000px; height: 200px">
+  <div class="row ">
+    <div class="col-md-4">
+      <img src=<?php echo $row['image']?>  class="img-fluid rounded"/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $row['name']?></h5>
+        <input type="submit" class="btn" id="loginbtn" name="submit" value="View" onclick="location.href='courses.php?id=<?php echo $row['id'] ?>'">
+      </div>
+    </div>
+  </div>
+</div>
 
-                   
-                </div>
-                                
-            </div>
-        </div>
-
-    
+      
 <?php
 }
 ?>
+</body>
+</html>
