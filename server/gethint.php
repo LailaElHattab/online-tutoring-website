f174e1957b24a4679926b84037dcb09c700765b7
laila-elhattab <liko.hani@gmail.com>
2022-01-18 01:40:05 +0200

    errors

--- server/gethint.php
+++ server/gethint.php
@@ -1,14 +1,15 @@
+<html>
 <?php
 include_once 'database.php';
-$connect = mysqli_connect("localhost", "root","","IS3");
+$connect = mysqli_connect("localhost", "root", "", "IS3");
 $output = '';
-if(isset($_POST["query"]))
-{
+if (isset($_POST["query"])) {
 	$search = mysqli_real_escape_string($connect, $_POST["query"]);
 	$query = "
-	SELECT * from user where fname '%".$search."%'";
-	}
-	else{
+	SELECT * from user where fname '%" . $search . "%'";
+} else {
 	echo 'Data not found';
-	}
-	?>
+}
+?>
+
+</html>
