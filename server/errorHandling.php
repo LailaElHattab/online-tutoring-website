f039254e51f4b0d1e24094689a1a12551e798c32
laila-elhattab <liko.hani@gmail.com>
2022-01-17 16:15:33 +0200

    initial commit

--- server/errorHandling.php
+++ server/errorHandling.php
@@ -1,5 +1,23 @@
 <html>
 <?php
+function result($conn, $sql)
+{
+    try {
+        $result = $conn->query($sql);
+        if (!$result) {
+            throw new Exception("Oops something went wrong");
+        }
+    } catch (Exception $e) {
+        echo "<div style='width:1500px; text-align: center' class='alert alert-danger' role='alert'>" . $e->getMessage() . "</div>";
+        die();
+    }
+    return $result;
+}
+function customError($errno, $errstr)
+{
+    echo "<b>Error:</b> [$errno] $errstr<br>";
+    echo "Something went wrong";
+}
 function database($errno, $errstr)
 {
     error_log("Error: [$errno] $errstr", 1, "anmiustudent@gmail.com", "From: is3@gmail.com");
