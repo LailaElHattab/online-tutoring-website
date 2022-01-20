7d50b2be38d5b2879451c572b755386f724892b2
laila-elhattab <liko.hani@gmail.com>
2022-01-21 01:27:11 +0200

    adjusted nav bar

--- server/navbar.php
+++ server/navbar.php
@@ -11,7 +11,7 @@ include_once 'database.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> -->
 
 </head>
 
@@ -32,7 +32,7 @@ include_once 'database.php';
             <!--navbar links-->
             <div class="collapse navbar-collapse col-3 mx-5 mt-1" id="main-nav">
                 <!--Search bar -->
-                <div class="col-4 mx-5 mt-1">
+                <div class="col-3 mx-5 mt-1">
                     <!-- <div class="form-group">
                         <div class="input-group">
                             <span class="input-group-addon">Search</span>
