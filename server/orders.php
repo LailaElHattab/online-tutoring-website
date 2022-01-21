cdc78c6ea07d8104f4bc0d293ed86e4480389c3f
Yosr Gamal <yosr.elqott@gmail.com>
2022-01-21 20:36:59 +0200

    made card

--- server/orders.php
+++ server/orders.php
@@ -19,7 +19,43 @@ session_start();
 
     <title>Orders</title>
 
+    <style>
+    :root{
+      --primary: #5B25FF;
+      --dark: rgb(17, 16, 16);
+      --body: #363EE0;
+      --dark2: #1c1d1f;
+      --box-shadow: 2px 3px 5px grey;
+      --active:#FFA700;
+      --font:"Open Sans", sans-serif;
+    }
+    @media (min-width:768px) {
+      #OrderView {
+        display: flex;
+        width: 700px;
+      }
+    }
+
+    #backg {
+      background-color: white;
+      padding-right: 400px;
+    }
+
+    #Orderbody {
+      background-image: url(../images/loginbackground.png);
+      background-repeat: no-repeat;
+      background-position-x: left;
+      background-position-y: 150px;
+      background-size: 400px;
+    }
+    #orderT{
+        color:var(--primary);
+        font-family:var(--font);
+    }
+  </style>
+
 </head>
+<body id= "Orderbody">
 <?php
 include 'database.php';
 include 'nav.php';
@@ -27,9 +63,15 @@ include 'functions.php';
 $sql = "SELECT * FROM purchaseItem WHERE purchase_id='" . $_GET['id'] . "'";
 $result = $conn->query($sql);
 ?>
-
-<table class="table caption-top ms-3 table-hover">
-    <caption>List of Orders</caption>
+  <section class="vh-100 d-flex justify-content-center" id="backg">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-md-12 col-xl-4">
+          <div class="card" style="border-radius: 20px;" id="OrderView">
+            <div class="card-body text-center">
+                <h1 id="orderT">Orders</h1>
+              <div class="mt-3 mb-4">
+<table class="table caption-top mx-3 table-hover">
     <thead>
         <tr>
             <th scope="col">Course ID</th>
@@ -51,5 +93,12 @@ $result = $conn->query($sql);
         }
         ?>
 </table>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
 
 </html>
