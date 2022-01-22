cf090426abf82a2c6dd0c1c2d439abbe3aea2c26
laila-elhattab <liko.hani@gmail.com>
2022-01-22 02:09:40 +0200

    added validation

--- server/addCourse.php
+++ server/addCourse.php
@@ -26,7 +26,7 @@ if ($_SESSION['user'] == 1) {
         el3.innerHTML = "Tutor Email:";
 
         el4 = document.createElement("input");
-        el4.setAttribute("type", "text");
+        el4.setAttribute("type", "email");
         el4.setAttribute("class", "form-control");
         el4.setAttribute("name", "email");
         el4.setAttribute("placeholder", "email");
@@ -42,26 +42,30 @@ if ($_SESSION['user'] == 1) {
 }
 
 if (isset($_POST["add"])) {
+
     $answer = true;
     $found = true;
 
     if ($_SESSION['user'] == 1) {
-
-        $sql1 = "select email from user where email='" . $_POST['email'] . "'";
-        $result1 = $conn->query($sql1);
-        if ($result1->num_rows > 0) {
-            $found = true;
-        } else {
+        $email1 = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        if (!filter_var($email1, FILTER_VALIDATE_EMAIL) === false) {
+            $sql1 = "select email from user where email='" . $email1 . "'";
+            $result1 = $conn->query($sql1);
+            if ($result1->num_rows > 0) {
+                $found = true;
+            } else {
 
     ?>
-            <div class='alert alert-danger col-md-4' style='width:300px;position:absolute;left:70%;top:78%'>
-                <label> Oops, This tutor is not registered </label><br><br>
-            </div>
+                <div class='alert alert-danger col-md-4' style='width:300px;position:absolute;left:70%;top:78%'>
+                    <label> Oops, This tutor is not registered </label><br><br>
+                </div>
         <?php
-            $found = false;
+                $found = false;
+            }
         }
     }
-    $sql = "select name from course where name='" . $_POST['name'] . "'";
+    $courseName = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $sql = "select name from course where name='" . $courseName . "'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         ?>
@@ -83,10 +87,10 @@ if (isset($_POST["add"])) {
     $target_dir = "images/";
     $target_file = $target_dir . basename($_FILES["imageToUpload"]["name"]);
     $allowed = array('image/gif', 'image/png', 'image/jpg', 'image/jpeg');
-    if (!in_array($_FILES["imageToUpload"]["type"], $allowed)) {
+    if (!in_array($_FILES["imageToUpload"]["type"], $allowed) || $_FILES['imageToUpload']['size'] > (2 * (1048576))) {
         ?>
         <div class='alert alert-danger col-md-4' style='width:300px;position:absolute;left:70%;top:95%'>
-            <label> please upload a file of image type e.g jpeg, png </label>
+            <label> please upload a file of image type e.g jpeg, png with a max size of 2 MB</label>
         </div>
     <?php
         $answer = false;
@@ -95,10 +99,10 @@ if (isset($_POST["add"])) {
     $target_dir1 = "courseInfo/";
     $target_file1 = $target_dir1 . basename($_FILES["contentToUpload"]["name"]);
     $allowed1 = array('text/plain', 'text/rtf');
-    if (!in_array($_FILES["contentToUpload"]["type"], $allowed1)) {
+    if (!in_array($_FILES["contentToUpload"]["type"], $allowed1) || $_FILES['contentToUpload']['size'] > (2 * (1048576))) {
     ?>
         <div class='alert alert-danger col-md-4' style='width:300px;position:absolute;left:70%;top:125%'>
-            <label> please upload a file of text type e.g plain, rtf </label>
+            <label> please upload a file of text type e.g plain, rtf with a max size of 2 MB</label>
         </div>
     <?php
         $answer = false;
@@ -106,10 +110,10 @@ if (isset($_POST["add"])) {
 
     $target_file2 = $target_dir1 . basename($_FILES["desToUpload"]["name"]);
 
-    if (!in_array($_FILES["desToUpload"]["type"], $allowed1)) {
+    if (!in_array($_FILES["desToUpload"]["type"], $allowed1) || $_FILES['desToUpload']['size'] > (2 * (1048576))) {
     ?>
         <div class='alert alert-danger col-md-4' style='width:300px;position:absolute;left:70%;top:110%'>
-            <label> please upload a file of text type e.g plain, rtf </label>
+            <label> please upload a file of text type e.g plain, rtf with a max size of 2 MB</label>
         </div>
     <?php
         $answer = false;
@@ -120,7 +124,6 @@ if (isset($_POST["add"])) {
         move_uploaded_file($_FILES["imageToUpload"]["tmp_name"], $target_file);
         move_uploaded_file($_FILES["contentToUpload"]["tmp_name"], $target_file1);
         move_uploaded_file($_FILES["desToUpload"]["tmp_name"], $target_file2);
-        $name = $_POST['name'];
         $image = $target_file;
         $content = $target_file1;
         $des = $target_file2;
@@ -139,11 +142,11 @@ if (isset($_POST["add"])) {
             $row2 = $result2->fetch_assoc();
             $tutor_id = $row2['id'];
             $status = 1;
-            $sql4 = "INSERT INTO course (name,image,content,description,level,price,rating,status,category,tutor_id,admin_id) VALUES ('$name','$image','$content','$des','$level','$price','0','$status','$category_id','$tutor_id','$admin_id')";
+            $sql4 = "INSERT INTO course (name,image,content,description,level,price,rating,status,category,tutor_id,admin_id) VALUES ('$courseName','$image','$content','$des','$level','$price','0','$status','$category_id','$tutor_id','$admin_id')";
         } else if ($_SESSION['user'] == 4) {
             $tutor_id = $_SESSION['id'];
             $status = 0;
-            $sql4 = "INSERT INTO course (name,image,content,description,level,price,rating,status,category,tutor_id) VALUES ('$name','$image','$content','$des','$level','$price','0','$status','$category_id','$tutor_id')";
+            $sql4 = "INSERT INTO course (name,image,content,description,level,price,rating,status,category,tutor_id) VALUES ('$courseName','$image','$content','$des','$level','$price','0','$status','$category_id','$tutor_id')";
         }
 
         $conn->query($sql4);
